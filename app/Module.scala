eeab976770101238422767c8d1f59dd848796d14
Rob G <crx8490@gmail.com>
2022-10-10 16:33:47 +0100

    added bindings in module

--- app/Module.scala
+++ app/Module.scala
@@ -1,5 +1,13 @@
 import com.google.inject.AbstractModule
+import services._
 
 class Module extends AbstractModule {
-  override def configure(): Unit = ???
+  override def configure(): Unit = {
+    bind(classOf[TeamService])
+      .to(classOf[MemoryTeamService])
+    bind(classOf[PlayerService])
+      .to(classOf[MemoryPlayerService])
+    bind(classOf[StadiumService])
+      .to(classOf[MemoryStadiumService])
+  }
 }
