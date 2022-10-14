734ab49c33b4c8ff583e814b993f6e8b8bbaf818
Rob G <crx8490@gmail.com>
2022-10-14 19:18:08 +0100

    updated TeamController for async

--- app/Module.scala
+++ app/Module.scala
@@ -1,17 +1,51 @@
-import com.google.inject.AbstractModule
+import com.google.inject.name.Named
+import com.google.inject.{AbstractModule, Provides}
+import org.mongodb.scala.{MongoClient, MongoDatabase}
+import play.api.Configuration
 import services._
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
-    bind(classOf[TeamService])
-      .to(classOf[MemoryTeamService]).in(classOf[javax.inject.Singleton])
-    bind(classOf[PlayerService])
-      .to(classOf[MemoryPlayerService]).in(classOf[javax.inject.Singleton])
-    bind(classOf[StadiumService])
-      .to(classOf[MemoryStadiumService]).in(classOf[javax.inject.Singleton])
+    @Provides
+    def databaseProvider(configuration: Configuration): MongoDatabase = {
+      val user = configuration.get[String]("mongo.username")
+      val pass = configuration.get[String]("mongo.password")
+      val dbName = configuration.get[String]("mongo.database")
+      val database = {
+        MongoClient(s"mongodb://$user:$pass@localhost:27017").getDatabase(dbName)
+      }
+      database
+    }
+
+    @Provides
+    @Named("stadiumCollection")
+    def stadiumCollectionProvider(db: MongoDatabase) = {
+      db.getCollection("stadiums")
+    }
+
+    @Provides
+    @Named("teamCollection")
+    def teamCollectionProvider(db: MongoDatabase) = {
+      db.getCollection("teams")
+    }
+
+    @Provides
+    @Named("playerCollection")
+    def playerCollectionProvider(db: MongoDatabase) = {
+      db.getCollection("players")
+    }
+
+//    bind(classOf[TeamService])
+//      .to(classOf[MemoryTeamService]).in(classOf[javax.inject.Singleton])
+//    bind(classOf[PlayerService])
+//      .to(classOf[MemoryPlayerService]).in(classOf[javax.inject.Singleton])
+//    bind(classOf[StadiumService])
+//      .to(classOf[MemoryStadiumService]).in(classOf[javax.inject.Singleton])
     bind(classOf[AsyncStadiumService])
       .to(classOf[MongoStadiumService]).in(classOf[javax.inject.Singleton])
     bind(classOf[AsyncPlayerService])
       .to(classOf[MongoPlayerService]).in(classOf[javax.inject.Singleton])
+    bind(classOf[AsyncTeamService])
+      .to(classOf[MongoTeamService]).in(classOf[javax.inject.Singleton])
   }
 }
