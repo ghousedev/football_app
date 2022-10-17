f654a9486ec0ad93bcf1bbb5285328e29c486ac4
Rob G <crx8490@gmail.com>
2022-10-17 14:20:54 +0100

    change test cases to match models

--- test/services/MemoryTeamServiceSpec.scala
+++ test/services/MemoryTeamServiceSpec.scala
@@ -10,32 +10,32 @@ class MemoryTeamServiceSpec extends PlaySpec {
   "MemoryTeamService" must {
     "return the size of the list after I create the team" in {
       val memoryTeamService = new MemoryTeamService()
-      val team = Team(15L, "Grimsby", Stadium(new Random().nextLong(), "Blundell Park", "Grimsby", "England", 10))
+      val team = Team(15L, "Grimsby", 10L)
       memoryTeamService.create(team)
       memoryTeamService.findAll().size mustBe 1
     }
     "find a team by an id and return that team if it is in there" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", Stadium(10L, "Blundell Park", "Grimsby", "England", 10))
+      val team = Team(15L, "Grimsby", 10L)
       memoryTeamService.create(team)
       val result = memoryTeamService.findById(team.id)
       result mustBe Some(team)
     }
     "find a team by an id that doesn't exist" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", Stadium(10L, "Blundell Park", "Grimsby", "England", 10))
+      val team = Team(15L, "Grimsby", 10L)
       memoryTeamService.create(team)
       val result = memoryTeamService.findById(10L)
       result mustBe Option.empty
     }
     "updating a team must change the values" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", Stadium(10L, "Blundell Park", "Grimsby", "England", 10))
+      val team = Team(15L, "Grimsby", 10L)
       memoryTeamService.create(team)
-      val updatedTeam = Team(15L, "Grimsby", Stadium(10L, "Blundell Park", "Grimsby", "England", 12))
+      val updatedTeam = Team(15L, "Grimsby", 10L)
       memoryTeamService.update(updatedTeam)
       val result = memoryTeamService.findById(updatedTeam.id).get
-      result mustBe Team(15L, "Grimsby", Stadium(10L, "Blundell Park", "Grimsby", "England", 12))
+      result mustBe Team(15L, "Grimsby", 10L)
       memoryTeamService.findAll().size mustBe 1
     }
 
