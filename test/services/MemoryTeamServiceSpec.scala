e13abaf0c58f4a16742b8ad7efa4a984b8ada894
Rob G <crx8490@gmail.com>
2022-10-20 17:28:59 +0100

    fix unit tests for new Team model

--- test/services/MemoryTeamServiceSpec.scala
+++ test/services/MemoryTeamServiceSpec.scala
@@ -10,32 +10,32 @@ class MemoryTeamServiceSpec extends PlaySpec {
   "MemoryTeamService" must {
     "return the size of the list after I create the team" in {
       val memoryTeamService = new MemoryTeamService()
-      val team = Team(15L, "Grimsby", 10L)
+      val team = Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.create(team)
       memoryTeamService.findAll().size mustBe 1
     }
     "find a team by an id and return that team if it is in there" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", 10L)
+      val team = Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.create(team)
       val result = memoryTeamService.findById(team.id)
       result mustBe Some(team)
     }
     "find a team by an id that doesn't exist" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", 10L)
+      val team = Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.create(team)
       val result = memoryTeamService.findById(10L)
       result mustBe Option.empty
     }
     "updating a team must change the values" in {
       val memoryTeamService = new MemoryTeamService
-      val team = Team(15L, "Grimsby", 10L)
+      val team = Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.create(team)
-      val updatedTeam = Team(15L, "Grimsby", 10L)
+      val updatedTeam = Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.update(updatedTeam)
       val result = memoryTeamService.findById(updatedTeam.id).get
-      result mustBe Team(15L, "Grimsby", 10L)
+      result mustBe Team(15L, "Grimsby", 10L, "https://someurl.com")
       memoryTeamService.findAll().size mustBe 1
     }
 
