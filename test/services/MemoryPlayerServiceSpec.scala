463a6f4554a56f52f0b77f374c67ef66e942f06f
stephenheath101 <98922374+stephenheath101@users.noreply.github.com>
2022-10-21 14:00:02 +0100

    player tests Fix

--- test/services/MemoryPlayerServiceSpec.scala
+++ test/services/MemoryPlayerServiceSpec.scala
@@ -1,38 +1,38 @@
 package services
 
-import models.{GoalKeeper, Player, Stadium, Team}
+import models.{GoalKeeper, Player}
 import org.scalatestplus.play.PlaySpec
 
 class MemoryPlayerServiceSpec extends PlaySpec {
   "MemoryPlayerService" must {
     "return the size of the list after I create the player" in {
       val memoryPlayerService = new MemoryPlayerService()
-      val player = Player(10L, 15L ,GoalKeeper, "Ian", "Grim")
+      val player = Player(10L, 15L ,GoalKeeper, "Ian", "Grim", "imgUrl")
       memoryPlayerService.create(player)
       memoryPlayerService.findAll().size mustBe 1
     }
     "find a Player by an id and return that Player if it is in there" in {
       val memoryPlayerService = new MemoryPlayerService
-      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim")
+      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim", "imgUrl")
       memoryPlayerService.create(player)
       val result = memoryPlayerService.findById(player.id)
       result mustBe Some(player)
     }
     "find a Player by an id that doesn't exist" in {
       val memoryPlayerService = new MemoryPlayerService
-      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim")
+      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim", "imgUrl")
       memoryPlayerService.create(player)
       val result = memoryPlayerService.findById(11L)
       result mustBe Option.empty
     }
     "updating a Player must change the values" in {
       val memoryPlayerService = new MemoryPlayerService
-      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim")
+      val player = Player(10L, 15L, GoalKeeper, "Ian", "Grim", "imgUrl")
       memoryPlayerService.create(player)
-      val updatedPlayer = Player(10L, 15L, GoalKeeper, "Mister", "Grim")
+      val updatedPlayer = Player(10L, 15L, GoalKeeper, "Mister", "Grim", "imgUrl")
       memoryPlayerService.update(updatedPlayer)
       val result = memoryPlayerService.findById(updatedPlayer.id).get
-      result mustBe Player(10L, 15L, GoalKeeper, "Mister", "Grim")
+      result mustBe Player(10L, 15L, GoalKeeper, "Mister", "Grim", "imgUrl")
       memoryPlayerService.findAll().size mustBe 1
     }
   }
